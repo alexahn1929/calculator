cfdf95f02e2b7841d35e649515cede2aa5e62a0f
alexahn1929 <121832249+alexahn1929@users.noreply.github.com>
2023-04-17 14:57:33 -0400

    Create button interface
    Add buttons that call the appropriate callback functions for the
    calculator when pressed.

--- calc.js
+++ calc.js
@@ -11,12 +11,23 @@ function operate(n1, operator, n2) {
     }
 }
 
-let hasOperator = false;
-let canConcat = true;
+const MAX_DISPLAY_LEN = 10;
+let hasOperator;
+let canConcat;
 let storedOperator;
 let storedNumber;
 
 let display = document.querySelector("#display");
+
+function negate(placeholder) {
+
+}
+
+function clear(placeholder) {
+    display.textContent = "0";
+    canConcat = true;
+    hasOperator = false;
+}
 
 function pressOperator(operator) {
     if (hasOperator) {
@@ -29,7 +40,7 @@ function pressOperator(operator) {
 
 function pressNumber(digit) {
     if (canConcat) {
-        if (display.textContent.includes(".")) { //can't add a decimal if there is already a decimal
+        if (digit === "." && display.textContent.includes(".")) { //can't add a decimal if there is already a decimal
             return;
         } else if (display.textContent === "0" && (digit !== "." || digit === "0")) { //can't add a numeric digit or another 0 after 0 -- remove the leading zero
             display.textContent = digit;
@@ -37,11 +48,39 @@ function pressNumber(digit) {
             display.textContent += digit;
         }
     } else {
-        display.textContent = "0";
-        canConcat = true;
+        clear();
         pressNumber(digit);
     }
 }
+
+//layout: div for digits, div for operator
+
+let callbacks = {};
+let DIGITS = "0123456789.";
+let OPERATORS = "+-*/=";
+for (let c of DIGITS) {
+    callbacks[c] = pressNumber;
+}
+for (let c of OPERATORS) {
+    callbacks[c] = pressOperator;
+}
+callbacks["+/-"] = negate;
+callbacks["Clear"] = clear;
+
+let container = document.querySelector("#container");
+let buttonLayout = [["7", "8", "9", "+"], ["4", "5", "6", "-"], ["1", "2", "3", "*"], ["+/-", "0", ".", "/"], ["=", "Clear"]];
+for (let r of buttonLayout) {
+    let row = document.createElement("div");
+    for (let btnVal of r) {
+        let btn = document.createElement("button");
+        btn.textContent = btnVal;
+        btn.addEventListener("click", (e) => callbacks[btnVal](e.target.textContent));
+        row.appendChild(btn);
+    }
+    container.appendChild(row);
+}
+
+clear();
 
 //maintain string as number
 //store an operator and one number, boolean hasOperator
@@ -54,5 +93,6 @@ function pressNumber(digit) {
 can't concat numbers to the result of an operation - only if the number started at 0
 can you parse a number that ends in a decimal?
 round numbers to a certain max number of decimal places
+don't let the user enter characters if the display is too full
 
 */
